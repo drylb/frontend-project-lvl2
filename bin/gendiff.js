fb8ab75fce60c8f47130f2086169494b9aede277
vladsergy <drylb924@gmail.com>
2021-01-13 21:38:30 +0200

    Arguments and option added using commander, Step 2.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ const { program } = commander;
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
-
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format[type]', 'output fromat');
 program.parse(process.argv);
